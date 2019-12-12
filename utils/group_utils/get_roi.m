94a3b7edbd3954d5264f32816678dbea9f1cc48a
Anne-Sophie Dubarry <asdubarry@gmail.com>
2019-12-12 11:51:23 +0100

    Vectorize creation of ROI from m_table_effect (only one loop over unique regions left) Makes flip signals a function Thorough clean of the function

--- utils/group_utils/get_roi.m
+++ utils/group_utils/get_roi.m
@@ -10,27 +10,27 @@ function [roi] = get_roi(m_table_effect,t, s, smask, all_labels,freqs,opt)
 % ***********************************************************************
 %
 % 2015/5/15 : ASD creation
-% ? : ASD modif flip according to description : 
-% We computed the correlation matrix across all signals within one region (i.e. across contacts and patients).
-% This matrix was thresholded separately for positive and negative values, based on a threshold chosen by the user (default thr = -0.7). 
-% We counted the number of high negative correlation (-thr) and pick the channel with the highest number of anti-correlated channels.
+% 2017 : ASD modif flip according to description : 
+% We computed the correlation matrix across all signals within one region 
+%   (i.e. across contacts and patients).
+%   This matrix was thresholded separately for positive and negative values, 
+%   based on a threshold chosen by the user (default thr = -0.7). 
+%   We counted the number of high negative correlation (-thr) 
+%   and pick the channel with the highest number of anti-correlated channels.
 % All channels highly correlated with this channel were flipped. 
+%
 % 2018/12/7 : correct flip : do not flip if nothing needs to be flipped! 
+%
+% 2019/10/28 : ASD : remove idx to count region - useless
 
 id_loc2 =5; 
 id_lat=4;
 starting_freq=6  ;
 id_subj = 1 ;
 roi = [] ;
-n  = 2; % Nombre de contact min par roi
 
 % Create unique ROI name
 [un,~,ic] = unique(strcat(m_table_effect(:,id_lat),'.',m_table_effect(:,id_loc2)),'stable');
-
-% Hist des occurences
-[N,X]= hist(ic,length(un));
-
-idx=find(N>=n) ; 
 
 if ~isempty(opt.freq)
     
@@ -53,158 +53,134 @@ if ~isempty(opt.freq)
 end
 
 % Get unique occurence of patients
-[unsubj, ~,icsubj]=unique(m_table_effect(:,id_subj)) ; 
+[unsubj, ~,~]=unique(m_table_effect(:,id_subj)) ; 
 
 % Distinct color for subj
 clr = hsv(numel(unsubj));  
 
 % ROI counter
-ct2 = 1; 
+ctRoi = 1; 
 
+% Filters contacts (or not) based on significance of signal
 if opt.signifmode ~=0
     bool_signif = cell2mat(m_table_effect(:,starting_freq+opt.freq)) ; 
 else
-     bool_signif = ones(size(m_table_effect,1),1) ;
+    bool_signif = ones(size(m_table_effect,1),1) ;
 end
 
-% For all region that has more than N contacts 
-for jj=1:length(idx)
+% For all unique regions 
+for jj=1:length(un)
 
-    % Boolean  for one region
-    bool_roi = ismember(strcat(m_table_effect(:,id_lat),'.',m_table_effect(:,id_loc2)),un(idx(jj))) ; 
+    % Boolean for one region
+    bool_roi = ismember(strcat(m_table_effect(:,id_lat),'.',m_table_effect(:,id_loc2)),un(jj)) ; 
 
-    % If at leaste two patients have active contacts 
+    % Gets patients which contribute to this region
     subj_active =unique(m_table_effect(logical(bool_roi.*bool_signif),id_subj)) ; 
-  
-    if length(subj_active)>=opt.nPt
-
-        str_roi = cell2mat(un(idx(jj))) ; 
-       
-        % Initialization
-        ct = 1;
-        subj_in = [];
-        masked_sig = [] ;
-        labels_roi = [] ;
-        m_res_roi = [] ; 
-        m_bool_roi = [] ; 
-        all_sig = [] ; 
-        idpt = [] ; 
-        
-        % Loop over all subjs
-        for ii=1:length(unsubj)
-
-            % All contacts present a region for a pt
-            ind_roi_subj = logical((ii==icsubj).*bool_roi.*bool_signif); 
-
-            % If this suject has an activation in this roi
-            if sum(ind_roi_subj~=0)
-                
-                % Subj idx
-                subj_in(ct) = ii ; 
-                
-                % Filter out double occurences
-                [c,ia,ic] =  unique(m_table_effect(ind_roi_subj,6)) ; 
-                idx1 = find(ind_roi_subj~=0);
-                
-                % Pick signals
-                sig = s(idx1(ia),:,opt.freq)';
-                
-                % Pick significativity masks
-                masked_sig = cat(2,masked_sig,smask(idx1(ia),:,opt.freq)');
-                
-                % Pick labels
-                current_labels = all_labels(idx1(ia)) ; 
-                labels_roi = cat(1,labels_roi,current_labels);
-             
-                m_bool_roi = cat(1,idx1(ia),m_bool_roi);
-                
-                % Mean signals per patients
-                if strcmp(opt.signmode,'signed') 
-                    mean_sig_subj(:,ct) = mean(sig,2) ; 
-                else 
-                    mean_sig_subj(:,ct) = mean(abs(sig),2) ; 
-                end
-                
-                % Concatenates all contacts signals
-                all_sig = cat(2,all_sig,sig);
-                idpt = cat(1, idpt, repmat(ct,size(sig,2),1)) ;                 
-        
-                % Increase subj indice
-                ct=ct+1; 
-    
-            end
-
-        end
-        
-        % Find onset of the first significant period
-        d = sum(masked_sig',1) ; 
-        detect = diff(d) ; 
-        onset = find(detect~=0,1) ; 
-        roi{ct2}.onset= t(onset) ; 
- 
-        % Compute correlations between contacts
-        r = corrcoef(all_sig) ; 
-                
-        % If FLIP option is used 
-        if isfield(opt, 'flip_thresh')
-           
-           % Select min in R 
-           A = (r<opt.flip_thresh) ; 
-           Ap =  (r>-opt.flip_thresh) ; 
    
-           % Finds ind with anti-correlation 
-           [Y,I] = max(sum(A)) ;
-           
-           % Nothing to flip
-           if Y ==0; I = []; end
-           fl = ones(1,length(A)) ; 
-           fl(find(Ap(I,:))) = -1 ; 
-             
-           %flip other signs to positively
-           % correlate with the frst timserie
-           all_sigFl= all_sig.*fl;    
-             
-           % Recompute correlations between FLIPPED contacts
-           r = corrcoef(all_sigFl) ; 
-           all_sig = all_sigFl ;
-           
-           % Recompute means poer patients
-           for ss=1:max(idpt) 
-                mean_sig_subj(:,ss) = mean(all_sig(:,idpt==ss),2);
-           end
-           
-           masked_sig = masked_sig.*fl ;
-        
-           % Add _FLP at the end of the contacts labels that were flipped
-           labels_roi(fl==-1) = strcat(labels_roi(fl==-1),'_FLP') ; 
-
+    % Do not take into account regions which have less patients than nPt 
+    if length(subj_active)<opt.nPt ; continue ; end
+   
+    % Removes doublons and gets contacts : involved in ROI / significant
+    [c,ia,ic] =  unique(m_table_effect(logical(bool_roi.*bool_signif),6)) ; 
+    
+    % Gets indices with doulbons
+    idx_signals_doublons = find(logical(bool_roi.*bool_signif)) ; 
+    
+    % Gets indices without doublons
+    idx_signals = idx_signals_doublons(ia) ; 
+ 
+    all_sig = s(idx_signals,:,opt.freq)';
+    masked_sig = smask(idx_signals,:,opt.freq)';
+    labels_roi = all_labels(idx_signals);
+    subj_in = find(ismember(unsubj,subj_active)); 
+ 
+    % Compute means per patients
+    [~,~,IC] = unique(cellfun( @(x) x(1:3), labels_roi, 'UniformOutput',false ));
+    for ss=1:max(IC) 
+        % Mean signals per patients
+        if strcmp(opt.signmode,'signed') 
+            mean_sig_subj(:,ss) = mean(all_sig(:,IC==ss),2); 
+        else 
+            mean_sig_subj(:,ss) = mean(abs(all_sig(:,IC==ss)),2) ; 
         end
-        
-        % Compute correlations between patients : interpatient correlation
-       rPt = corrcoef(mean_sig_subj) ; 
-       trPt = tril(rPt,-1) ; 
-       roi{ct2}.corrPt = mean(trPt(trPt~=0)); 
-       roi{ct2}.allCorrPt = trPt(trPt~=0);        
-       
-       % Compute correlations between channels : intra+inter patients
-       trChan = tril(r,-1) ; 
-       roi{ct2}.corrChan =  mean(trChan(trChan~=0)); 
-       roi{ct2}.allCorrPt = trChan(trChan~=0);
-       
-       roi{ct2}.name =  cell2mat(un(idx(jj))) ; 
-       roi{ct2}.signmoy = mean_sig_subj ;
-       roi{ct2}.Fmask = masked_sig ;
-       roi{ct2}.labels = labels_roi ;
-       roi{ct2}.idPt = subj_in ; 
-       roi{ct2}.namePt = unsubj(subj_in) ; 
-       roi{ct2}.t = t ; 
-       roi{ct2}.freq= freqs{opt.freq} ; 
-       
-       ct2=ct2+1;
+     end
 
-        clear rC trC ptCor chanCor r tr mean_sig_subj A ; 
-
+    % If FLIP option is used 
+    if isfield(opt, 'flip_thresh')
+          [mean_sig_subj,masked_sig,labels_roi,r] = flip_signals(all_sig, masked_sig, labels_roi, opt.flip_thresh) ; 
+    else
+           r = corrcoef(all_sig) ; 
  
     end
 
+    % Compute correlations between patients : interpatient correlation
+    rPt = corrcoef(mean_sig_subj) ; 
+    trPt = tril(rPt,-1) ; 
+    roi{ctRoi}.corrPt = mean(trPt(trPt~=0)); 
+    roi{ctRoi}.allCorrPt = trPt(trPt~=0);        
+
+    % Compute correlations between channels : intra+inter patients       
+    trChan = tril(r,-1) ; 
+    roi{ctRoi}.corrChan =  mean(trChan(trChan~=0)); 
+    roi{ctRoi}.allCorrChan = trChan(trChan~=0);
+   
+    % Find onset of the first significant period
+    d = sum(masked_sig',1) ; 
+    detect = diff(d) ; 
+    onset = find(detect~=0,1) ; 
+    roi{ctRoi}.onset= t(onset) ; 
+
+    % All other fields to roi 
+    roi{ctRoi}.name =  cell2mat(un(jj)) ; 
+    roi{ctRoi}.signmoy = mean_sig_subj ;
+    roi{ctRoi}.Fmask = masked_sig ;
+    roi{ctRoi}.labels = labels_roi ;
+    roi{ctRoi}.idPt = subj_in ; 
+    roi{ctRoi}.namePt = unsubj(subj_in) ; 
+    roi{ctRoi}.t = t ; 
+    roi{ctRoi}.freq= freqs{opt.freq} ; 
+
+    ctRoi=ctRoi+1;
+
+    clear r mean_sig_subj ; 
+
 end
+
+% This function flip signals if option is checked (for broadband "LFP"signals) 
+function  [mean_sig_subj,masked_sig,labels_roi,r]  =  flip_signals(all_sig, masked_sig, labels_roi, flip_thresh)
+
+    % Computes correlations
+    r = corrcoef(all_sig) ; 
+
+    % Select min in R 
+    A = (r<flip_thresh) ; 
+    Ap =  (r>-flip_thresh) ; 
+
+    % Finds ind with anti-correlation 
+    [Y,I] = max(sum(A)) ;
+
+    % Nothing to flip
+    if Y ==0; I = []; end
+
+     % Create a vector which indicate if flip is done (-1) or not (+1) 
+    fl = ones(1,length(A)) ; 
+    fl(find(Ap(I,:))) = -1 ; 
+
+    % Flip signals
+    all_sigFl= all_sig.*fl;    
+    
+    % Flip masks of significance
+    masked_sig = masked_sig.*fl ;
+
+    % Recompute correlations between FLIPPED contacts
+    r = corrcoef(all_sigFl) ; 
+    all_sig = all_sigFl ;
+
+    % Recompute means per patients
+    [C,IA,IC] = unique(cellfun( @(x) x(1:3), labels_roi, 'UniformOutput',false )) ;
+    for ss=1:max(IC) 
+        mean_sig_subj(:,ss) = mean(all_sig(:,IC==ss),2);
+    end
+    
+    % Add _FLP at the end of the contacts labels that were flipped
+    labels_roi(fl==-1) = strcat(labels_roi(fl==-1),'_FLP') ; 
